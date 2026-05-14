4993708c8d07ffb94741b9be6caaeba59bbd819e
Jayvee Madriaga Nacino <kolehiyongsubic.ph@gmail.com>
2026-05-14 09:33:54 +0800

    index HEAD -> main, origin/main

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ const notesRoutes = require('./routes/notesRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://entrance-exam-web.vercel.app", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
